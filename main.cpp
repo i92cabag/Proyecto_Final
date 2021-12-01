bcfe44d3a0bc21fc8eb61c47246ff809ac80db0c
i92caelj <91150622+i92caelj@users.noreply.github.com>
2021-12-01 10:48:22 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include "maquina.hpp"
+#include "cluster.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+	cout << "Bienvenido al software de reserva de maquinas de la Universidad de Cordoba" << endl;
+	cout << endl;
+
+}
