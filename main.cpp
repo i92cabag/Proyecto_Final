41e1446f7cca4076f1ae0ef197b9c50f1f6a676d
i92caelj <91150622+i92caelj@users.noreply.github.com>
2021-12-04 13:25:42 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 
 #include "maquina.hpp"
 #include "usuario.hpp"
+#include "reservas.hpp"
 #include "cluster.hpp"
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
+
+void reserva_maquina( vector<Maquina> &maquinas, vector<Reserva> &reservas, vector<Usuario> usuarios ){
+
+cout << "Las maquinas disponibles actualmente son las siguientes: " << endl;
+mostrar_maquinas_disponibles( maquinas );
+cout << endl;
+cout << "Rellene los datos de la reserva que quiere llevar a cabo: " << endl;
+Reserva aux = rellenar_reserva( maquinas, usuarios );
+if( modificar_maquina( maquinas, aux.get_maquina_reserva(), aux.get_cantidad_reserva() ) == true ){
+
+	reservas.push_back( aux );
+	cout << "Reserva realizada" << endl;
+	}
+else{
+
+	cout << "La reserva no se pudo completar debido a que exige una mayor cantidad de cpu que la que la maquina tiene disponible." << endl;
+	cout << "Se le recomiendan las siguientes maquinas: " << endl;
+	 
+	mostrar_maquinas_recomendadas( maquinas, aux.get_maquina_reserva(), aux.get_cantidad_reserva() );
+	}
+
+}
 
 
 int main(){
@@ -22,40 +46,43 @@ vector< Usuario > usuarios;
 rellenar_usuarios( usuarios );
 usuarios.resize( usuarios.size() - 1 );
 
-for( size_t i = 0; i < usuarios.size(); i++  ){
+vector< Reserva > reservas;
+rellenar_reservas( reservas );
+reservas.resize( reservas.size() - 1 );
 
-	cout << "Usuario: " << i << endl;
-	cout << "Usuario id: " <<usuarios[i].get_id() << "  Usuario nombre: " << usuarios[i].get_nombre() << "  Usuario contraseña: " << usuarios[i].get_contrasena() << "  Usuario email: " <<  usuarios[i].get_email() << "  Usuario departamento: " << usuarios[i].get_departamento() << " Usuario limite res: " << usuarios[i].get_lim_reservas() << "  Usuario limite cpu: " << usuarios[i].get_lim_cpu() << endl;
-	
-	}
-
-//vector< Reserva > reservas;
-//rellenar_reservas( reservas );
-
-remove("maquinas.txt");
-remove("usuarios.txt");
-
+int estado;
 do{
 
-	cout << "Seleccinoa si eres un investigador o un administrador: " << endl;
+	cout << "Selecciona si eres un investigador o un administrador: " << endl;
 	cout << endl;
 	cout << "1. Investigador" << endl;
 	cout << "2. Administrador" << endl;
+	cout << "3. salir del sistema" << endl;
 
-	int estado;
 	cin >> estado;
-	if( estado != 1 || estaddo != 2 ){
+	if( estado < 1 || estado > 3 ){
 	
+		cout << endl;
 		cout << "Selecciona una de las dos opciones." << endl;
 		}
-	}while( estado != 1 || estado != 2 );
+	}while( estado < 1 || estado > 3 );
 	
 if( estado == 1 ){
 
 	cout << "Bienvenido a tu zona de trabajo investigador, selecciona que accion quieres llevar a cabo." << endl;
-	
+	cout << endl;
 	cout << "1. realizar la reserva de una maquina" << endl;
-	reserva();
+	
+	int accion;
+	
+	cin >> accion;
+	
+	switch(accion){
+	
+		case 1:
+			reserva_maquina( maquinas, reservas, usuarios );
+		
+		}
 	}
 
 if( estado == 2 ){
@@ -65,75 +92,17 @@ if( estado == 2 ){
 	
 	}
 
+remove("maquinas.txt");
+remove("usuarios.txt");
+remove("reservas.txt");
 
 volcar_maquinas_fichero( maquinas );
 
 volcar_usuarios_fichero( usuarios );
 
-//volcar_reservas_fichero( reservas );
+volcar_reservas_fichero( reservas );
 
 }
-
-
-
-
-/*
-void rellenar_reservas( vector< Maquina > &reservas ){
-
-string nombrefichero = "reservas.txt";
-string usuario_id, maquina, cpu_reservada, descripcion;
-ifstream fichero;
-fichero.open( nombrefichero, ifstream::in );
-
-if( fichero.is_open() == false ){
-	
-	cout << "error al abrir el archivo de reservas.txt" << endl;
-	abort();
-	}
-Maquina aux;
-while( !fichero.eof() ){
-	
-	fichero >> usuario_id;
-	fichero >> maquina;
-	fichero >> cpu_reservada;
-	getline( fichero, descripcion );
-	
-	aux.cambiar_usuario_id( stoi( usuario_id) );
-	aux.cambiar_maquina( stoi( maquina ) );
-	aux.cambiar_cpu_reservada( stoi( cpu_reservada ) );
-	aux.cambiar_descripcion( descripcion );
-	
-	reservas.push_back( aux );	
-	
-	}
-fichero.close();
-}
-
-
-void volcar_reservas_fichero( vector<Reserva> reservas ){
-
-string nombrefichero = "reservas.txt";
-ofstream fichero;
-fichero.open( nombrefichero, ofstream::out );
-
-if( fichero.is_open() == false ){
-
-	cout << "el fichero reservas.txt no se pudo abrir correctamente" << endl;
-	abort();
-	}
-for( size_t i = 0; i < maquinas.size() -1; i++ ){
-	
-	fichero << reservas[i].get_usuarioid() << " " << reservas[i].get_maquina() << " " << reservas[i].get_cantidad_reservada() << endl;
-	fichero << reservas[i].get_descripcion() << endl;
-	}
-fichero.close();
-
-
-}
-
-*/
-
-
 
 
 
