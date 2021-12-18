546896bcaa8f251d721eed1dfae0a26c566964a8
i92caelj <91150622+i92caelj@users.noreply.github.com>
2021-12-18 13:51:37 +0100

    Add files via upload

--- maquina.hpp
+++ maquina.hpp
@@ -1,3 +1,4 @@
+
 #ifndef maquina_hpp
 #define maquina_hpp
 
@@ -59,18 +60,48 @@ class Maquina{
 
 };
 
+/**
+@brief funcion que vuelca del fichero donde se guardan los datos de las maquinas a un vector de maquinas con el que se va a trabajar.
+@param maquinas el vector donde se van a guardar los datos de todas las maquinas.
+*/
 void rellenar_maquinas( vector< Maquina > &maquinas );
 
+/**
+@brief funcion que devuelve el vector de maquinas despues de haber trabajado con el al fichero de maquinas para su futura lectura cuando se vuelva a abrir el programa.
+@param maquinas el vector del que se van a leer las maquinas que se van a pasar al fichero.
+*/
 void volcar_maquinas_fichero( vector< Maquina > maquinas );
 
+/**
+@brief funcion que muestra las maquinas que tienen todavía cpu sin utilizar.
+@param maquinas vector de maquinas.
+*/
 void mostrar_maquinas_disponibles( vector<Maquina> maquinas );
 
+/**
+@brief funcion que modifica la cantidad libre de cpu de una maquina.
+@param maquinas vector de todas las maquinas.
+@param maquina entero identificativo de la maquina
+@param cantidad_cpu cantidad de cpu que va a tener libre la nueva maquina.
+*/
 bool modificar_maquina( vector<Maquina> &maquinas, int maquina, int cantidad_cpu );
 
+/**
+@brief funcion que muestra las maquinas que se le recomiendasn al usuario para su uso con respecto a la cpu que pide el usuario.
+@param cantidad_cpu cantidad de cpu que el usuario pide de una maquina.
+*/
 void mostrar_maquinas_recomendadas( vector<Maquina> maquinas, int maquina, int cantidad_cpu );
 
+/**
+@brief funcion que devuelve un booleano indicando si existe la maquina que el usuario ha indicado.
+@param maquina entero identificativo de la maquina.
+*/
 bool maquina_existe( vector<Maquina> maquinas, int maquina );
 
+/**
+@brief funcion que devuelve el cluster en el que se encuentra una maquina.
+@param maquina entero identificativo de la maquina.
+*/
 int get_cluster( vector<Maquina> maquinas, int maquina );
 
 #endif
--- reservas.hpp
+++ reservas.hpp
@@ -48,19 +48,45 @@ class Reserva {
 		}
 };
 
-
+/**
+@brief funcion que vuelca del fichero donde se guardan los datos de las reservas a un vector de reservas con el que se va a trabajar.
+@param reservas el vector donde se van a guardar los datos de todas las reservas.
+*/
 void rellenar_reservas( vector< Reserva > &reservas );
 
+/**
+@brief funcion que devuelve el vector de reservas despues de haber trabajado con el al fichero de reservas para su futura lectura cuando se vuelva a abrir el programa.
+@param reservas el vector del que se van a leer las reservas que se van a pasar al fichero.
+*/
 void volcar_reservas_fichero( vector<Reserva> reservas );
 
+/**
+@brief funcion que devuelve una reserva que ha rellenado el usuario.
+@param maquinas vector de maquinas que se le va a mostrar al usuario para que elija una de ellas para su utilizacion.
+@param usuarios vector de usuarios que se va a utilizar para comprobar que usuario realiza la reserva.
+*/
 Reserva rellenar_reserva( vector<Maquina> maquinas, vector<Usuario> usuarios );
 
+/**
+@brief funcion que muestra todas las reservas de un usuario.
+@param codigo_usuario id del usuario del que se van a mostrar las reservas.
+*/
 void mostrar_reservas( vector<Reserva> reservas, int codigo_usuario );
 
+/**
+@brief funcion que muestra todas las reservas de un usuario.
+*/
 void mostrar_reservas( vector<Reserva> reservas );
 
+/**
+@brief funcion que comprueba que una maquina esta reservada o no por un usuario.
+@param codigo_maquina id de la maquina de la que se va a comprobar si esta reservada o no por el usuario.
+*/
 bool comprobar_maquina_reservada( vector<Reserva> reservas, int codigo_usuario, int codigo_maquina );
 
+/**
+@brief funcion que elimina una reserva de un usuario.
+*/
 void eliminar_reserva_usuario( vector<Reserva> &reservas, vector<Maquina> &maquinas, int codigo_usuario, int codigo_maquina );
 
 #endif
