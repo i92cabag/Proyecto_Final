a5a182b85a601fef562070c5fd16ae489617e591
CarlosCord26 <55401105+CarlosCord26@users.noreply.github.com>
2021-12-18 16:37:25 +0100

    Añadido pruebas maquina_existe

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,4 +1,3 @@
-
 #include "reservas.cpp"
 #include "maquina.cpp"
 #include "usuario.cpp"
@@ -55,7 +54,49 @@ ASSERT_EQ( false, comprobar_maquina_reservada( reservas, 100, 2 ) );
 
 }
 
+TEST( maquina_existeTest, positive ){
 
+std::vector<Maquina> maquinas;
+
+Maquina aux;
+aux.modificar_cpu_disp( 1 );
+aux.modificar_cluster( 3 );
+aux.modificar_cpu_maquina(4);
+aux.modificar_id_maquina(5);
+maquinas.push_back(aux);
+
+aux.modificar_cpu_disp( 2 );
+aux.modificar_cluster( 6 );
+aux.modificar_cpu_maquina(5);
+aux.modificar_id_maquina(9);
+maquinas.push_back(aux);
+
+ASSERT_EQ( true, maquina_existe( maquinas, 5) );
+ASSERT_EQ( true, maquina_existe( maquinas, 9) );
+
+}
+
+TEST( maquina_existeTest, negative ){
+
+std::vector<Maquina> maquinas;
+
+Maquina aux;
+aux.modificar_cpu_disp( 8 );
+aux.modificar_cluster( 7 );
+aux.modificar_cpu_maquina(6);
+aux.modificar_id_maquina(6);
+maquinas.push_back(aux);
+
+aux.modificar_cpu_disp( 9 );
+aux.modificar_cluster( 8 );
+aux.modificar_cpu_maquina(10);
+aux.modificar_id_maquina(10);
+maquinas.push_back(aux);
+
+ASSERT_EQ( false, maquina_existe( maquinas, 7) );
+ASSERT_EQ( false, maquina_existe( maquinas, 11) );
+
+}
 
 int main(){
 
